fc798617f9b32ae87a0209cce11516943993e1a6
Pupkin <rasraziel@gmail.com>
2021-06-28 22:01:26 +0200

    Heroku deploy

--- src/connections.js
+++ src/connections.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // Localhost
-const baseURL ='https://triangle-back.herokuapp.com/'
-// const baseURL ='http://localhost:8080/'
+// const baseURL ='https://triangle-back.herokuapp.com/'
+const baseURL ='http://localhost:8080/'
 
 
 export default axios.create({
